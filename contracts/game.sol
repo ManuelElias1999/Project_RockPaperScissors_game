62bd8947b03e986fa477f9dbc84133de558c08c9
Manuel Elias <elias.soria.juan.manuel@gmail.com>
2024-04-24 22:11:38 -0400

    Upload code

--- contracts/game.sol
+++ contracts/game.sol
@@ -8,53 +8,112 @@ contract RockPaperScissors is ERC721, Ownable {
     enum Move { None, Rock, Paper, Scissors }
 
     struct Game {
-        address player;
-        Move playerMove;
-        Move contractMove;
+        address player1;
+        address player2;
+        Move player1Move;
+        Move player2Move;
         bool played;
     }
 
     mapping(uint256 => Game) private games;
-    uint256 public gameId = 0;
+    uint256 public gameId = 1;
 
-    event GamePlayed(address player, uint256 gameId, Move playerMove, Move contractMove, address winner, string result);
+    event GameCreated(uint256 gameId, address player1);
+    event GameJoined(uint256 gameId, address player2);
+    event GamePlayed(uint256 gameId, address player, Move move);
+    event GameFinished(uint256 gameId, address winner, string result);
 
     constructor() ERC721("RockPaperScissorsToken", "RPS") Ownable(msg.sender) {}
 
-    function play(uint256 _move) external {
-        require(_move >= 1 && _move <= 3, "Invalid move");
-        require(balanceOf(msg.sender) == 0, "You already played a game");
+    function createGame() external returns (uint) {
+        games[gameId].player1 = msg.sender;
 
-        Move playerMove = Move(_move);
-        Move contractMove = Move(random());
-        address winner = determineWinner(playerMove, contractMove);
-        string memory result;
+        emit GameCreated(gameId, msg.sender);
 
-        if (winner == msg.sender) {
-            _safeMint(msg.sender, gameId);
-            result = "You win!";
-        } else if (winner == address(0)) {
-            result = "It's a tie!";
-        } else {
-            result = "You lose!";
-        }
-
-        games[gameId] = Game(msg.sender, playerMove, contractMove, true);
-        emit GamePlayed(msg.sender, gameId, playerMove, contractMove, winner, result);
-
-        gameId++;
+        return gameId++;
     }
 
-    function determineWinner(Move _playerMove, Move _contractMove) internal view returns (address) {
-        if (_playerMove == _contractMove) {
-            return address(0); // Tie
-        } else if ((_playerMove == Move.Rock && _contractMove == Move.Scissors) ||
-                   (_playerMove == Move.Paper && _contractMove == Move.Rock) ||
-                   (_playerMove == Move.Scissors && _contractMove == Move.Paper)) {
-            return msg.sender; // Player wins
+    function joinGame(uint256 _gameId) external {
+        require(games[_gameId].player1 != address(0), "Game does not exist");
+        require(games[_gameId].player2 == address(0), "Game already has two players");
+        require(games[_gameId].player1 != msg.sender, "You cannot join your own game");
+
+        games[_gameId].player2 = msg.sender;
+
+        emit GameJoined(_gameId, msg.sender);
+    }
+
+    function playGame(uint256 _move) external {
+        require(games[gameId - 1].player1 != address(0), "Game does not exist");
+        require(games[gameId - 1].played == false, "Game has already been played");
+        require(_move >= 1 && _move <= 3, "Invalid move");
+        require(msg.sender == games[gameId - 1].player1 || msg.sender == games[gameId - 1].player2, "You are not allowed to play this game");
+
+        if (msg.sender == games[gameId - 1].player1) {
+            games[gameId - 1].player1Move = Move(_move);
         } else {
-            return address(this); // Contract wins
+            games[gameId - 1].player2Move = Move(_move);
         }
+
+        emit GamePlayed(gameId - 1, msg.sender, Move(_move));
+
+        if (games[gameId - 1].player1Move != Move.None && games[gameId - 1].player2Move != Move.None) {
+            address winner;
+            string memory result;
+            if (games[gameId - 1].player1Move == games[gameId - 1].player2Move) {
+                winner = address(0); // Tie
+                result = "It's a tie!";
+            } else if (
+                (games[gameId - 1].player1Move == Move.Rock && games[gameId - 1].player2Move == Move.Scissors) ||
+                (games[gameId - 1].player1Move == Move.Paper && games[gameId - 1].player2Move == Move.Rock) ||
+                (games[gameId - 1].player1Move == Move.Scissors && games[gameId - 1].player2Move == Move.Paper)
+            ) {
+                winner = games[gameId - 1].player1; // Player 1 wins
+                result = "Player 1 wins!";
+                _safeMint(winner, gameId - 1);
+            } else {
+                winner = games[gameId - 1].player2; // Player 2 wins
+                result = "Player 2 wins!";
+                _safeMint(winner, gameId - 1);
+            }
+
+            emit GameFinished(gameId - 1, winner, result);
+
+            
+
+            games[gameId - 1].played = true;
+        }
+    }
+
+        function playAgainstMachine(uint256 _move) external {
+        require(_move >= 1 && _move <= 3, "Invalid move");
+
+        Move playerMove = Move(_move);
+        uint256 machineMove = random();
+
+        emit GamePlayed(0, msg.sender, playerMove);
+        emit GamePlayed(0, address(this), Move(machineMove));
+
+        address winner;
+        string memory result;
+        if (playerMove == Move(machineMove)) {
+            winner = address(0); // Tie
+            result = "It's a tie!";
+        } else if (
+            (playerMove == Move.Rock && Move(machineMove) == Move.Scissors) ||
+            (playerMove == Move.Paper && Move(machineMove) == Move.Rock) ||
+            (playerMove == Move.Scissors && Move(machineMove) == Move.Paper)
+        ) {
+            winner = msg.sender; // Player wins
+            result = "You win!";
+            _safeMint(msg.sender, 0);
+        } else {
+            winner = address(this); // Machine wins
+            result = "You lose!";
+            _safeMint(address(this), 0);
+        }
+
+        emit GameFinished(0, winner, result);
     }
 
     function random() internal view returns (uint256) {
